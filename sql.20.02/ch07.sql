04cddb93328c957d6a0deb80745ba531fc239d5a
SIST98 <SIST98@DESKTOP-VSOFP86>
2020-02-17 16:22:49 +0900

    a

--- sql.20.02/ch07.sql
+++ sql.20.02/ch07.sql
@@ -106,4 +106,62 @@ from employees emp
 where emp.employee_id not in (
           select mgr.manager_id
           from employees mgr
-          where manager_id is not null);                             
+          where manager_id is not null);   
+
+
+--실습
+--1.
+select last_name, hire_date
+from employees
+where department_id = (select department_id
+                        from employees
+                        where last_name = 'Abel')
+and last_name <> 'Abel';
+
+--2.
+select employee_id, last_name, salary
+from employees
+where salary > (select avg(salary) from employees)
+order by salary;
+
+--3.
+select employee_id, last_name
+from employees
+where department_id in (select department_id
+                        from employees
+                        where last_name like '%u%');
+
+--4.
+select last_name, department_id, job_id
+from employees
+where department_id in (select department_id
+                        from departments
+                        where location_id = 1700);
+
+--5.
+select last_name, salary
+from employees
+where manager_id = (select employee_id
+                    from employees
+                    where last_name = 'Kochhar');
+
+--6.
+select department_id, last_name, job_id
+from employees
+where department_id in (select department_id
+                    from departments
+                    where department_name = 'IT');
+
+--7.
+select last_name from employees
+where salary >any (select salary
+                    from employees
+                    where department_id = 60);
+
+--8.
+select employee_id, last_name, salary
+from employees
+where department_id in (select department_id
+                        from employees
+                        where last_name like '%u%')
+and salary > (select avg(salary) from employees);
